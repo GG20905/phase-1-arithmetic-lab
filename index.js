081522c3366113040a919eca7946f68467d7d50a
Githinji <ggathungu20905@gmail.com>
2024-03-19 11:39:29 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=31
+let num2=2
+let multiply=num1*num2
 
+const random=Math.floor(Math.random() * 10) + 1;
+//=> 1
+
+let num3=48
+let num4=11
+const mod=num3%num4;
+const max=Math.max(5,10,15,20);
 // Write your code here
